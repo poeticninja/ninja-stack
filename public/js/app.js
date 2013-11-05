38d81280a31a7ab13e3dd5c88a86d0e132f0af4d
Saul Maddox <saul.maddox@gmail.com>
2013-11-05 16:58:44 -0600

    Added two angular routes and links to those routes
    Just basic routes showing how angular routes work.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-//  *** CLIENT SIDE APP BASED ON ANGULAR SEED ***
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute','ngResource','ngAnimate']);
+angular.module('myApp', [
+	'ngRoute',
+	'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/view1', {templateUrl: 'views/partials/partial1.html', controller: 'MyCtrl1'});
+	$routeProvider.when('/view2', {templateUrl: 'views/partials/partial2.html', controller: 'MyCtrl2'});
+	$routeProvider.otherwise({redirectTo: '/view1'});
+}]);
