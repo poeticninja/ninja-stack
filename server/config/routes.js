1d66341d44eddeff36dba67bf0403a655393642d
Saul Maddox <saul.maddox@gmail.com>
2013-11-05 17:43:13 -0600

    Changes the default routes for angular partials to the server.
    Now angular partials are being pulled form the server side instead of
    the client side public folder.

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,6 +8,9 @@ module.exports = function(app) {
 	//Home route
 	app.get('/', routes.index);
 
+	// This routes any angular partials to the server directory. Not the Public. Comment or remove this line to handle the partials on the client side.
+	app.get('/views/partials/:name', routes.partials);
+
 	// redirect all others to the index (HTML5 history). Remove this if you don't want Angular to handle all routes.
 	app.get('*', routes.index);
 };
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,8 @@
 exports.index = function(req, res) {
 	res.render('index', {"title": "Ninja Stack"});
 };
+
+exports.partials = function (req, res) {
+	var name = req.params.name;
+	res.render('partials/' + name);
+};
