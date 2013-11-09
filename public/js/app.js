23010994890b1c4a89d10641e65518a7143bf968
Saul Maddox <saul.maddox@gmail.com>
2013-11-09 12:45:31 -0600

    Updated Angular view route

--- public/js/app.js
+++ public/js/app.js
@@ -7,5 +7,5 @@ var myApp = angular.module('myApp', ['ngRoute', 'ngResource', 'ngAnimate']);
 myApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/view1', {templateUrl: 'views/partials/partial1.html', controller: 'MyCtrl1'});
 	$routeProvider.when('/view2', {templateUrl: 'views/partials/partial2.html', controller: 'MyCtrl2'});
-	$routeProvider.otherwise({redirectTo: '/'});
+	$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
